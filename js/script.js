f1b97b707fd24be89124a12829a6810229718982
Arifeen Hossain <arifeenh18@gmail.com>
2021-08-18 17:09:52 +0600

    created

---
+++ js/script.js
@@ -0,0 +1,25 @@
+document.getElementById("inputs").addEventListener("click", function (event) {
+  const number = event.target.innerText;
+  const resultInput = document.getElementById("result-value");
+  const previousNumber = resultInput.innerText;
+  const newNumber = previousNumber + number;
+  if (number == "=" && previousNumber.includes("+")) {
+    resultInput.innerText = eval(resultInput.innerText);
+    return;
+  } else if (number == "=" && previousNumber.includes("-")) {
+    resultInput.innerText = eval(resultInput.innerText);
+    return;
+  } else if (number == "=" && previousNumber.includes("*")) {
+    resultInput.innerText = eval(resultInput.innerText);
+    return;
+  } else if (number == "=" && previousNumber.includes("/")) {
+    resultInput.innerText = eval(resultInput.innerText);
+    return;
+  } else if (number == "." && previousNumber.includes(".")) {
+    return;
+  } else if (number == "C") {
+    resultInput.innerText = "";
+  } else {
+    resultInput.innerText = newNumber;
+  }
+});
